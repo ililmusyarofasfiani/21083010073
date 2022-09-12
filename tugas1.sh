4a1fdb8dfb00867d5bb58185e4f7831447aceac5
ililmusyarofasfiani <iyilmusyawa@gmail.com>
2022-09-12 19:47:58 +0700

    tugas1.sh

---
+++ tugas1.sh
@@ -0,0 +1,7 @@
+echo "Tugas 1 sistem oprasi kelas B"
+echo "................................"
+echo "nama : ilil musyarof asfiani"
+echo "Npm :21083010073"
+echo "................................"
+echo "Hallo Dunia"
+echo "Ini adalah skrip Bash pertamaku di linux"
